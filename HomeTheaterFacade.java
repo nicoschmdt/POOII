a6c2caad061cc47c2e557b11e38043c78d740687
Nicole <45316185+nicoschmdt@users.noreply.github.com>
2019-06-04 10:36:02 -0300

    Add files via upload

---
+++ HomeTheaterFacade.java
@@ -0,0 +1,15 @@
+
+public class HomeTheaterFacade {
+	Amplifier amp;
+	Tuner tuner;
+	DvdPlayer dvd;
+	CdPlayer cd;
+	Projector projector;
+	TheaterLights lights;
+	Screen screen;
+	PopcornPopper popper;
+	
+	public void watchMovie(String movie){
+		System.out.println("Get ready to watch a movie...");
+	}
+}
