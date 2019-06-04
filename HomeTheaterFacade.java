b1459397ce5239a7e2545e400d59bfd7e773224f
Nicole <45316185+nicoschmdt@users.noreply.github.com>
2019-06-04 10:37:56 -0300

    Update HomeTheaterFacade.java

--- HomeTheaterFacade.java
+++ HomeTheaterFacade.java
@@ -21,6 +21,16 @@ public final class HomeTheaterFacade {
 	
 	public void watchMovie(String movie){
 		System.out.println("Get ready to watch a movie...");
-		
+// 		popper.on();
+// 		popper.pop();
+// 		lights.dim(10);
+// 		screen.down();
+// 		projector.on();
+// 		projector.wideScreenMode();
+// 		amp.on();
+// 		amp.setSurroundSound();
+// 		amp.setVolume(5);
+// 		dvd.on();
+// 		dvd.play(movie);
 	}
 }
