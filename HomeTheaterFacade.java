3e5d26e7f2eb968c857b8a41338f3fc55741c125
Nicole <45316185+nicoschmdt@users.noreply.github.com>
2019-06-04 10:36:31 -0300

    Update HomeTheaterFacade.java

--- HomeTheaterFacade.java
+++ HomeTheaterFacade.java
@@ -1,15 +1,26 @@
 
-public class HomeTheaterFacade {
-	Amplifier amp;
-	Tuner tuner;
-	DvdPlayer dvd;
-	CdPlayer cd;
-	Projector projector;
-	TheaterLights lights;
-	Screen screen;
-	PopcornPopper popper;
+public final class HomeTheaterFacade {
+	
+	private static final HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade();
+//	Amplifier amp;
+//	Tuner tuner;
+//	DvdPlayer dvd;
+//	CdPlayer cd;
+//	Projector projector;
+//	TheaterLights lights;
+//	Screen screen;
+//	PopcornPopper popper;
+	
+	private HomeTheaterFacade(){
+		System.err.println("HomeTheaterFacade object created.");
+	}
+	
+	public static HomeTheaterFacade getInstance(){
+		return homeTheaterFacade;
+	}
 	
 	public void watchMovie(String movie){
 		System.out.println("Get ready to watch a movie...");
+		
 	}
 }
